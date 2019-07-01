c2844b1a000523b1000f23a9c84b0bdcf45cd418
Salim Fadhley <salimfadhley@gmail.com>
2019-07-01 12:53:56 +0100

    Refactor this demo

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import logging
 from guizero import App, Text
-app = App(title="Hello world")
-message = Text(app, text="Welcome to the Hello world app!")
-app.display()
+
+log: logging.Logger = logging.getLogger()
+
+def make_app()->App:
+    app = App(title="Hello world")
+    message = Text(app, text="Welcome to the Hello world app!")
+    return app
+
+
+def main():
+    logging.basicConfig()
+    logging.getLogger("").setLevel(logging.INFO)
+
+    log.info("Starting the app!")
+    app:App = make_app()
+    app.display()
+
+if __name__ == "__main__":
+    main()
